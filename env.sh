5551cdafce7f6ba60e27f78bc51fcf8d1be78934
yuanliang <zylxjtu@users.noreply.github.com>
2023-06-27 16:26:50 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,22 +3,17 @@
 # This is to export environment variables that will be shared by a couple of shell scripts
 # Every cluster specific configuration should be set/update here before running the scripts
 
-#export subscription="WAMS-TEST-StreamingTeam"
-#export subscriptonid="1b447bc0-050f-4d85-b2b8-d79a631e9750"
-export subscription="yuanliang"
-export subscriptonid="b922e3c4-f2ae-4867-9699-16b6158acebf"
-export resourcegroup="containerTest"
-export clustername="wincontainertest"
-export nodepoolname="npwin"
-export location="westus"
-export nodevmsize="Standard_D2s_v3"
-export managedidentity="containerTestMI"
+export subscription="Replace with your value"
+export subscriptonid="Replace with your value"
+export resourcegroup="Replace with your value"
+export clustername="Replace with your value"
+export nodepoolname="Replace with your value"
+export location="Replace with your value"
+export nodevmsize="Replace with your value"
+export managedidentity="Replace with your value"
 
 export managedidentityresourceid="/subscriptions/$subscriptonid/resourcegroups/containerTest/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$managedidentity"
 
 # Do not need to care about the lines below if you are not creating new clusters
-# Replace the password with the actual one when you deploy
-export WINDOWS_USERNAME="containertest"
-
-# Replace the password with the real one
-export WINDOWS_PASSWORD="Abcd123456789@"
+export WINDOWS_USERNAME="Replace with your value"
+export WINDOWS_PASSWORD="Replace with your value"
