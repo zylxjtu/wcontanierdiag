ce59f42d1c0b93dbae80d94ab93cfee0e76a48ad
yuanliang <zylxjtu@users.noreply.github.com>
2023-07-03 18:09:22 -0700

    Update startmonagent.sh

--- startmonagent.sh
+++ startmonagent.sh
@@ -6,17 +6,11 @@ hostprocesspod=hpc
 # Retrieve the credentials of existing cluster
 #az aks get-credentials --resource-group $resourcegroup --name $clustername --overwrite-existing
 
-# Fill the deployment yaml file with correct parameters
-sed -i -e "s,<managedidentityresourceid>,${managedidentityresourceid}," \
-       -e "s/<subscription>/${subscription}/" \
-       -e "s/<resourcegroup>/${resourcegroup}/" \
-       -e "s/<clustername>/${clustername}/" \
-       -e 's/\r//g' \
-       ./${hostprocesspod}.yaml
-
 # Deploy the monagent hpc container and start the monagnet process
 kubectl apply -f ./${hostprocesspod}.yaml --wait
 
 kubectl wait --for=condition=Ready --all --timeout -1s pods
 
 kubectl get pods -o wide
+
+kubectl exec -it ${hostprocesspod} -- powershell ./startMonagent.ps1 $managedidentityresourceid $subscription $resourcegroup $clustername 1.19
