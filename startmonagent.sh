be37e6623be1bbdd237d7a92e840df8d73f4bea3
yuanliang <zylxjtu@users.noreply.github.com>
2023-08-01 13:58:18 -0700

    Update startmonagent.sh

--- startmonagent.sh
+++ startmonagent.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+source env.sh
 hostprocesspod=hpc
 # Need to login first
 #az login
 #az account set --subscription $subscription
+
+# This can be removed after we pushed the HPC image to a public repository
+az aks update -n ${clustername} -g $resourcegroup --attach-acr $registryname
 
 # Retrieve the credentials of existing cluster
 #az aks get-credentials --resource-group $resourcegroup --name $clustername --overwrite-existing
