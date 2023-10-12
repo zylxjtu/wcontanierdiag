e0d325a18a31bdef5fc2393bfbb5e719f8cd017f
yuanliang <zylxjtu@users.noreply.github.com>
2023-10-12 16:25:36 -0700

    Update startmonagent.sh

--- startmonagent.sh
+++ startmonagent.sh
@@ -5,26 +5,26 @@
 
 source env.sh
 
-hostprocesspod=hpc
+hostprocessdaemonset=hpc
 # Need to login first
 #az login
-az account set --subscription $subscription
+#az account set --subscription $subscription
 
 # Retrieve the credentials of existing cluster
 #az aks get-credentials --resource-group $resourcegroup --name $clustername --overwrite-existing
 
 # Deploy the monagent hpc container and start the monagnet process
-az aks update -n ${clustername} -g $resourcegroup --attach-acr $registryname
+#az aks update -n ${clustername} -g $resourcegroup --attach-acr $registryname
 
-sed -e "s,\$managedidentityresourceid,$managedidentityresourceid," \
-    -e "s/\$subscription/$subscription/" \
-    -e "s/\$resourcegroup/$resourcegroup/" \
-    -e "s/\$clustername/$clustername/" \
+sed -e "s,\$managedidentityresourceid,$MANAGEDIDENTITY_RESOURCE_ID," \
+    -e "s/\$subscription/$AZURE_SUBSCRIPTION_ID/" \
+    -e "s/\$resourcegroup/$RESOURCE_GROUP/" \
+    -e "s/\$clustername/$CLUSTER_NAME/" \
     -e 's/\r//g' \
-    ./${hostprocesspod}.yaml.template > ${hostprocesspod}.yaml
+    ./${hostprocessdaemonset}.yaml.template > ${hostprocessdaemonset}.yaml
 
-kubectl apply -f ./${hostprocesspod}.yaml --wait
+#kubectl apply -f ./${hostprocessdaemonset}.yaml --wait
 
-kubectl wait --for=condition=Ready --all --timeout -1s pods
+#kubectl wait --for=condition=Ready --all --timeout -1s daemonsets
 
-kubectl get pods -o wide
+#kubectl get daemonsets -o wide
